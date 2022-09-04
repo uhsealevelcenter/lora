6e607b370d6eb948c8bed79e9109377a09822947
derekyoung <derek.young@gmail.com>
2022-09-03 22:21:13 -1000

    formatting and type checking

--- tektelic-orca-ttnmapper.js
+++ tektelic-orca-ttnmapper.js
@@ -1,10 +1,11 @@
 function decodeUplink(b) {
   var chan = b.bytes[0]; 
   var typeid = b.bytes[1];
-  var lat = (b.bytes[4] | b.bytes[3]<<8 | b.bytes[2]<<16 | (b.bytes[2] & 0x80 ? 0xFF<<24 : 0));
-  var lon = (b.bytes[8] | b.bytes[7]<<8 | b.bytes[6]<<16 | b.bytes[5]<<24 | (b.bytes[5] & 0x80 ? 0xFF<<32 : 0));
-  var alt = (b.bytes[10] | b.bytes[9]<<8 | (b.bytes[9] & 0x80 ? 0xFF<<16 : 0));
-
+  if (chan === 0x00 && typeid === 0x88) {
+     var lat = (b.bytes[4] | b.bytes[3]<<8 | b.bytes[2]<<16 | (b.bytes[2] & 0x80 ? 0xFF<<24 : 0));
+     var lon = (b.bytes[8] | b.bytes[7]<<8 | b.bytes[6]<<16 | b.bytes[5]<<24 | (b.bytes[5] & 0x80 ? 0xFF<<32 : 0));
+     var alt = (b.bytes[10] | b.bytes[9]<<8 | (b.bytes[9] & 0x80 ? 0xFF<<16 : 0));
+  }
   return {
     data: {
       chan: chan,
